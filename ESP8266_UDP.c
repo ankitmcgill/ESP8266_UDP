a9519418635684d9e3d7813e22873e70b9f4236d
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-06 13:48:39 +0530

    added udp send function

--- ESP8266_UDP.c
+++ ESP8266_UDP.c
@@ -35,10 +35,28 @@ void ESP8266_UDP_create_listener(uint16_t udp_port, espconn_recv_callback callba
 	error = espconn_regist_recvcb(&(h->esp_connection_variable), callback);
 }
 
+void ESP8266_UDP_send_data_ip(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4, uint16_t port, uint8_t* data, uint16_t data_len, struct ESP8266_UDP_HANDLE* h)
+{
+	//SEND THE SPECIFIED DATA OF THE SPECIFIED LENGTH TO THE REMOTE IP: PORT
+	//SPECIFIED IN THE UDP HANDLE STRUCTURE
+
+	h->esp_connection_variable.type = ESPCONN_UDP;
+	h->esp_connection_variable.state = ESPCONN_NONE;
+	h->esp_connection_variable.proto.udp = &(h->esp_udp_variable);
+	IP4_ADDR((ip_addr_t*)h->esp_connection_variable.proto.udp->remote_ip, ip1, ip2, ip3, ip4);
+	h->esp_connection_variable.proto.udp->remote_port = port;
+
+	espconn_create(&(h->esp_connection_variable));
+	espconn_send(&(h->esp_connection_variable), data, data_len);
+	espconn_delete(&(h->esp_connection_variable));
+	os_free(h);
+}
+
 void ESP8266_UDP_delete(struct ESP8266_UDP_HANDLE* handle)
 {
 	//DELETE THE SPECIFIED UDP HANDLE (CONTAINING THE
 	//ESP8266 UDP CONNECTION VARIABLE
 
 	espconn_delete(&(handle->esp_connection_variable));
+	os_free(handle);
 }
--- ESP8266_UDP.h
+++ ESP8266_UDP.h
@@ -10,6 +10,7 @@
 #define _ESP8266_UDP_H_
 
 #include "user_interface.h"
+#include "mem.h"
 #include <ets_sys.h>
 #include <espconn.h>
 
@@ -22,6 +23,7 @@ struct ESP8266_UDP_HANDLE
 
 
 void ESP8266_UDP_create_listener(uint16_t udp_port, espconn_recv_callback callback, struct ESP8266_UDP_HANDLE* h);
+void ESP8266_UDP_send_data_ip(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4, uint16_t port, uint8_t* data, uint16_t data_len, struct ESP8266_UDP_HANDLE* h);
 void ESP8266_UDP_delete(struct ESP8266_UDP_HANDLE* handle);
 
 #endif
