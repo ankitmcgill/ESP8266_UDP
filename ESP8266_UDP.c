8d8e4d67889955fd0ca40a3616e7b4d2510f5e89
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-11 01:58:36 +0530

    added combined send_receive udp function

--- ESP8266_UDP.c
+++ ESP8266_UDP.c
@@ -11,9 +11,9 @@
 
 void ESP8266_UDP_create_listener(uint16_t udp_port, espconn_recv_callback callback, struct ESP8266_UDP_HANDLE* h)
 {
-	//CREATE A UDP LISTENER ON THE SPECIFIED PORT
-	//AND REGISTER A CALLBACK WITH THE SPECIFIED
+	//CREATE A UDP LISTENER ON THE SPECIFIED PORT AND REGISTER A CALLBACK WITH THE SPECIFIED
 	//FUNCTION POINTER
+	//THIS FUNCTION IS UNIDIRECTIONAL. ONLY TO RECEIVE DATA
 
 	//NOTE WHEN CREATING AND PASSING A POINTER TO STRUCTURE ESP8266_UDP_HANDLE FROM OTHER FILE, USE MALLOC/ZALLOC TO CREATE
 	//THE STRUCTURE AND PASS THE POINTER LIKE THIS
@@ -37,9 +37,10 @@ void ESP8266_UDP_create_listener(uint16_t udp_port, espconn_recv_callback callba
 
 void ESP8266_UDP_send_data_ip(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4, uint16_t port_remote, uint16_t port_local, uint8_t* data, uint16_t data_len, struct ESP8266_UDP_HANDLE* h)
 {
-	//SEND THE SPECIFIED DATA OF THE SPECIFIED LENGTH TO REMOTE IP: PORT
-	//THROUGH THE LOCAL PORT PASSED AS AN ARGUMENT
+	//SEND THE SPECIFIED DATA OF THE SPECIFIED LENGTH TO REMOTE IP: PORT THROUGH THE LOCAL PORT PASSED AS AN ARGUMENT
 	//SPECIFIED IN THE UDP HANDLE STRUCTURE
+	//THIS FUNCTION IS UNIDIRECTIONAL - ONLY TO SEND DATA
+	//UDP OBJECT DELETED AFTER JOB DONE
 
 	h->esp_connection_variable.type = ESPCONN_UDP;
 	h->esp_connection_variable.state = ESPCONN_NONE;
@@ -52,6 +53,29 @@ void ESP8266_UDP_send_data_ip(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4
 	espconn_send(&(h->esp_connection_variable), data, data_len);
 	espconn_delete(&(h->esp_connection_variable));
 	os_free(h);
+}
+
+void ESP8266_UDP_send_receive_data_ip(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4, uint16_t port_remote, uint16_t port_local, uint8_t* data, uint16_t data_len, espconn_recv_callback callback, struct ESP8266_UDP_HANDLE* h)
+{
+	//SEND THE SPECIFIED DATA OF THE SPECIFIED LENGTH TO REMOTE IP: PORT THROUGH THE LOCAL PORT PASSED AS AN ARGUMENT
+	//SPECIFIED IN THE UDP HANDLE STRUCTURE. THEN ALSO REGISTER A CALLBACK FUNCTION FOR THE REPLY ON THE SAME LOCAL PORT
+	//AS USED FOR SENDING
+	//THIS IS FOR BIDIRECTIONAL UDP
+	//UDP OBJCT NOT DELETED AFTER JOB DONE
+
+	h->esp_connection_variable.type = ESPCONN_UDP;
+	h->esp_connection_variable.state = ESPCONN_NONE;
+	h->esp_connection_variable.proto.udp = &(h->esp_udp_variable);
+	IP4_ADDR((ip_addr_t*)h->esp_connection_variable.proto.udp->remote_ip, ip1, ip2, ip3, ip4);
+	h->esp_connection_variable.proto.udp->local_port = port_local;
+	h->esp_connection_variable.proto.udp->remote_port = port_remote;
+
+	espconn_create(&(h->esp_connection_variable));
+
+	//REGISTER CALLBACK FUNCTION
+	espconn_regist_recvcb(&(h->esp_connection_variable), callback);
+
+	espconn_send(&(h->esp_connection_variable), data, data_len);
 }
 
 void ESP8266_UDP_delete(struct ESP8266_UDP_HANDLE* handle)
